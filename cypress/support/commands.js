df9e06a0443181d80412a00fdbc65e6a133d13c6
Guilherme <gdalbo1310@gmail.com>
2024-02-22 13:03:11 -0300

    first commit

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,15 @@
 import 'cypress-localstorage-commands'
+const initialTerm = 'React'
+const newTerm = 'Cypress'
 
 Cypress.Commands.add('assertLoadingIsShownAndHidden', () => {
   cy.contains('Loading ...').should('be.visible')
   cy.contains('Loading ...').should('not.exist')
+})
+
+Cypress.Commands.add('search', term => {
+  cy.get('input[type="text"]')
+    .should('be.visible')
+    .clear()
+    .type(`${term}{enter}`)
 })
